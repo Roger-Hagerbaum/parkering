7c8f079c11d752b64301730bc5e5aaafd0ba67eb
Roger-Hagerbaum <roger.hagerbaum@lerniastudent.se>
2023-05-26 12:39:09 +0200

    boilerplate

---
+++ src/test/java/com/rogerhagerbaum/parkerning/ParkerningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerhagerbaum.parkerning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkerningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
